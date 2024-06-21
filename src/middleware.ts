f6a20debbe9fbe0c9f0047472ce4a77a788141a5
vchirikov <vovanchig@gmail.com>
2024-06-21 23:44:45 +0300

    better description of the issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
 import type { MiddlewareHandler } from 'astro';
 
-export const onRequest: MiddlewareHandler = async (ctx, next) => {
-  const match = ctx.request.url.match(/\/(ru|foo)(\/.*)$/);
-  if (match) {
-    const rewriteTo = match[2];
-    const response = await ctx.rewrite(rewriteTo);
-    console.log('requested url', ctx.request.url, 'response from rewritten url', rewriteTo, response);
-    return response;
-  }
-  const response = await next();
-  console.log('requested url', ctx.request.url, 'response from default handler', response);
+export const onRequest: MiddlewareHandler = async (_ctx, next) => {
+  const response = await next('/');
+  console.log('expected response.status is', response.status);
   return response;
 };
